bc46e95e31cb4da8c04779529e0e4a019e738eb8
projectr4vival <projectr4vival@libero.it>
2021-06-04 14:18:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -123,8 +123,6 @@ var slides = document.getElementsByClassName("mySlides");
 showSlides();
 
 function showSlides() {
-  /*var i;
-  var slides = document.getElementsByClassName("mySlides");*/
 
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
@@ -134,4 +132,5 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
+  
 } 
