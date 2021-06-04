ba92e1b3514caca2d67dc5e459c7e59e567e55f5
projectr4vival <projectr4vival@libero.it>
2021-06-04 13:16:31 +0200

    Update

--- script.js
+++ script.js
@@ -127,9 +127,10 @@ function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
 
-  /*for (i = 0; i < slides.length; i++) {
+  for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
-  }*/
+  }
+  
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
