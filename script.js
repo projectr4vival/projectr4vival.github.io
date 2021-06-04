945657344b1e3dcee06787feade90990fd30e29c
projectr4vival <projectr4vival@libero.it>
2021-06-04 14:16:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -117,24 +117,18 @@ function showSlides(n) {
 
 
 var slideIndex = 0;
-/*var i;
-var slides = document.getElementsByClassName("mySlides");*/
+var i;
+var slides = document.getElementsByClassName("mySlides");
 
 showSlides();
 
 function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
+  /*var i;
+  var slides = document.getElementsByClassName("mySlides");*/
 
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  dots[slideIndex-1].className += " active";
 
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
