6186085e4049595a48cf1058e9017b5d09b58940
projectr4vival <projectr4vival@libero.it>
2021-06-04 14:10:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -130,6 +130,17 @@ function showSlides() {
     slides[i].style.display = "none";
   }
 
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  dots[slideIndex-1].className += " active";
+
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
