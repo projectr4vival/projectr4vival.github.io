c4b815a96ff182c66fedb4ee523381d3c8fd489a
projectr4vival <projectr4vival@libero.it>
2021-06-07 11:54:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -85,37 +85,7 @@ window.onclick = function(event) {
 } 
 
 
-/*Slideshow
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}*/
-
-
+// Slideshow
 var slideIndex = 0;
 var i;
 var slides = document.getElementsByClassName("mySlides");
@@ -137,6 +107,12 @@ function showSlides() {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  dots[slideIndex-1].className += " active";
 
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
