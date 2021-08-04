be190224e448a69f804d2963bfce1fdc24f5f946
projectr4vival <projectr4vival@libero.it>
2021-08-04 18:58:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ function showDropdown() {
 }
 
 // Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
+/*window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
@@ -95,7 +95,20 @@ window.onclick = function(event) {
       }
     }
   }
-} 
+} */
+
+window.addEventListener('click', function () {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+});
 
 
 // Slideshow
