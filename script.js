ffa2eba1b44dfaf1d74811a13c238c3e108ad12b
projectr4vival <davide.roxas@libero.it>
2021-09-30 11:20:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -255,6 +255,8 @@ var mybutton = document.getElementById("scrollButton");
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
+window.addEventListener("scroll", scrollFunction());
+
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
