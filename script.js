7d03fa094de2486e9a1dffd1771b3f8002ab7169
projectr4vival <davide.roxas@libero.it>
2021-10-01 21:10:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -249,17 +249,17 @@ function showMenu(x){
 
 /* ScrollButton */
 
-//Get the button:
-mybutton = document.getElementById("scrollButton");
+/*Get the button:
+mybutton = document.getElementById("scrollButton");*/
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    document.getElementById("scrollButton").style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    document.getElementById("scrollButton").style.display = "none";
   }
 }
 
