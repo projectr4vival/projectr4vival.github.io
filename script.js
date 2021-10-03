fb051981781bd0e508e459108be4bd3e75efc385
projectr4vival <davide.roxas@libero.it>
2021-10-03 12:02:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -252,7 +252,9 @@ function showMenu(x){
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
-window.addEventListener("scroll", scrollFunction());
+window.addEventListener("scroll", () => {
+  scrollFunction()
+});
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 || window.scrollY > 20) {
