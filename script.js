b71de0ef247fa8296156d7341bcf1a923accbac2
projectr4vival <davide.roxas@libero.it>
2021-10-03 11:44:56 +0200

    Fixing the scroll-to-the-top button script adding a statement

--- script.js
+++ script.js
@@ -249,17 +249,17 @@ function showMenu(x){
 
 /* ScrollButton */
 
-/*Get the button:
-mybutton = document.getElementById("scrollButton");*/
+/*Get the button */
+var mybutton = document.getElementById("scrollButton");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("scrollButton").style.display = "block";
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 || window.scrollY > 20) {
+    mybutton.style.display = "block";
   } else {
-    document.getElementById("scrollButton").style.display = "none";
+    mybutton.style.display = "none";
   }
 }
 
